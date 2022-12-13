86ccdac98ae68e97a7045d07962907a08b23bdd4
JenK1ss <103268453+JenK1ss@users.noreply.github.com>
2022-12-13 23:12:54 +0300

    Update V.py (полная хуйня).py

--- V.py (полная хуйня).py
+++ V.py (полная хуйня).py
@@ -1,1 +1,18 @@
+a = list(map(str,input().split()))
+b = []
+count = []
+a.sort()
+for i in range(len(a)):
+    for j in range(len(a)):
+        if a[i] == a[j]:
+            b.append(a[i])
+            count.append(a.count(a[i]))
+ans = dict(zip(b,count))
+sorted_dict = {}
+sorted_keys = reversed(sorted(ans, key=ans.get))
+
+for w in sorted_keys:
+    sorted_dict[w] = ans[w]
+
+print(sorted_dict)
 
