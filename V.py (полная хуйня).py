f76f87acc6e3b88615103c8b0378e0afc07a812f
JenK1ss <103268453+JenK1ss@users.noreply.github.com>
2022-12-13 21:44:58 +0300

    Update V.py (полная хуйня).py

--- V.py (полная хуйня).py
+++ V.py (полная хуйня).py
@@ -1,13 +1,1 @@
-a = list(map(str,input().split()))
-b = []
-count = []
-for i in range(len(a)):
-    for j in range(len(a)):
-        if a[i] == a[j]:
-            b.append(a[i])
-            count.append(a.count(a[i]))
-ans = dict(zip(b,count))
-values = list(ans.values())
-ans = list(ans)
-for g in range(len(ans)):
-    print(values[g],ans[g])
+
