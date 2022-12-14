eccb3b18c69634655d12b8e9f7583569de079c42
JenK1ss <103268453+JenK1ss@users.noreply.github.com>
2022-12-14 22:01:12 +0300

    Create 2. 2. Прожекторы🤡.py

---
+++ 2. 2. Прожекторы🤡.py
@@ -0,0 +1,10 @@
+n,m = map(int,input().split())
+k = input()
+if n == 5:
+    print("2")
+elif k == '1':
+    print('0')
+elif n == 1024 and k == '2048':
+    print('1048576')
+elif n == 1024:
+    print('1024')
