5b6540245713efbfb46c4624e7cc0ad83e6de208
JenK1ss <103268453+JenK1ss@users.noreply.github.com>
2022-12-17 14:20:49 +0300

    Update 05-12-04. Сломанный датчик.py

--- 05-12-04. Сломанный датчик.py
+++ 05-12-04. Сломанный датчик.py
@@ -1,1 +1,12 @@
+n = int(input())
+tem = []
+press = []
+for _ in range(n):
+    t,p = [float(i) for i in input().split()]
+    tem.append(t)
+    if -70 < t < 80:
+        press.append(p)
 
+s_tem = sum(tem)/len(tem)
+s_press = sum(press)/len(press)
+print(f'{s_tem:.4f} {s_press:.4f}')
